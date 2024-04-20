4161db99bcd5bf826c279c38dfb6cf133b2f1be4
iffatulfatima <157104458+iffatulfatima@users.noreply.github.com>
2024-04-20 21:45:03 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let answer=await inquirer.prompt([{
+    name:"sentence",
+    type:"input",
+    message:"Enter the sentence to find the no of words in it:"
+   }])
+   let sentenceToArr=(answer.sentence).trim()
+   let wordCount=0;
+   sentenceToArr=(answer.sentence).split(" ")
+   for (let i = 0; i < sentenceToArr.length; i++) {
+    wordCount++;
+    
+   }
+   console.log(wordCount);
+   
